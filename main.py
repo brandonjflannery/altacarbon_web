bfa787029ada6da082594e25f69fcd0ca0f8cb32
Brandon Flannery <brandonflannery@Brandons-MacBook-Pro.local>
2023-01-18 11:23:40 -0800

    Adding contact form updates and style improvements

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
+import smtplib
+import pandas as pd
+from dotenv import load_dotenv   #for python-dotenv method
+load_dotenv()
+import os
+
+### SMTP ARGS ###
+SMTP_HOST = 'smtp.mailtrap.io'
+SMTP_PORT = 587
+SMTP_USERNAME = 'd6f6e81a51fa39'
+SMTP_PASSWORD = '2769eb75e0fc0c';
+EMAIL_FROM = 'mailtrap@altacarbon.eco'
+EMAIL_TO = 'brandon.flannery@altacarbon.eco'
+#################
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	return app.send_static_file('index.html')
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        print(data)
+        send_email(data["name"], data["email"], data["subject"], data["message"])
+        return render_template('contactsuccess.html')
+    else:
+        return app.send_static_file('index.html')
+
+
+def send_email(name, email, subject, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nSubject: {subject}\nMessage:{message}"
+    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as connection:
+        connection.ehlo()
+        connection.starttls()
+        connection.login(SMTP_USERNAME, SMTP_PASSWORD)
+        connection.sendmail(EMAIL_FROM, EMAIL_TO, email_message)
 
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5029)
+    app.run(threaded=True, port=4121)
+
 
 
